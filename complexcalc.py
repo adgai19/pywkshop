50d36948ecdd3ea50ab4583bbc0dd384e029bac7
adgai19 <gaitondeaditya@gmail.com>
2020-02-12 22:50:09 +0530

    added documentation comments

--- complexcalc.py
+++ complexcalc.py
@@ -1,15 +1,21 @@
 def add(val1,val2):
+        """ Function to return sum of two complex numbers
+        input parameters:- 2 touples of length 2.
+        return value= sum of val2 and val1"""
         real=val1[0]+val2[0]
         complex=val1[1]+val2[1]
         return (real,complex)
+
 def sub(val1,val2):
         real=val1[0]-val2[0]
         complex=val1[1]-val2[1]
         return (real,complex)
+
 def mul(val1,val2):
         real=val1[0]*val2[0]-val1[1]*val2[1]
         complex=val1[0]*val2[1]+val1[1]*val2[0]
         return (real,complex)
+
 def conj(val):return (val[0],-val[1])
 
 def div(val1,val2):
@@ -17,3 +23,4 @@ def div(val1,val2):
         real= int[0]/(val2[0]**2+val2[1]**2)
         complex=int[1]/(val2[0]**2+val2[1]**2)
         return (real,complex)
+
--- complextest.py
+++ complextest.py
@@ -1,2 +1,3 @@
 from complexcalc import add
 print(add((1,1),(2,2)))
+print(add.__doc__)

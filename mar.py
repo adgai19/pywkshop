2fc097d786663e8e22ce0e894732845ba54c9533
adgai19 <gaitondeaditya@gmail.com>
2020-02-12 13:53:25 +0530

    prettify print

--- mar.py
+++ mar.py
@@ -1,14 +1,13 @@
 import requests
-import pandas as pd
 payload={
         'apikey':'13ee848c',
         't':'spider man homecoming'
 
 }
 response=requests.get("http://www.omdbapi.com",params=payload)
-
+print(response)
 print(response.status_code)
-print(response.json())
+#print(response.json())
 
 data=response.json()
 print(type(data))
@@ -16,3 +15,5 @@ print(type(data))
 print(data["Title"])
 #print(data.title)
 #print(pd.Dataframe(response.json()))
+for x,y in data.items():
+        print(x,":-",y)

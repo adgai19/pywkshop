d56120ad50e9a408a1ef0b0c7f2a6f320490c7a9
adgai19 <gaitondeaditya@gmail.com>
2020-02-14 21:48:37 +0530

    list done

--- pyapi.py
+++ pyapi.py
@@ -2,4 +2,12 @@ import requests
 response = requests.get("http://api.open-notify.org/astros.json")
 print(response.status_code)
 print(response.json())
-print(response.content)
+#print(response.content)
+people=response.json()["people"]
+
+
+print(people)
+print("-"*100,"\n"*5)
+print("Craft\tName")
+for li in people:
+  print(li['craft'],"\t",li["name"])

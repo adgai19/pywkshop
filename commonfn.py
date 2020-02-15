8112098c38ec9fc2d82f4011354992bdf81de9ea
adgai19 <gaitondeaditya@gmail.com>
2020-02-15 15:23:35 +0530

    added common functions and added docstring

---
+++ commonfn.py
@@ -0,0 +1,17 @@
+a = 'Hello!'
+
+
+print(len(a))
+
+print(a.upper())
+no=-3.8
+print(abs(no))
+print(round(no))
+list=3,2,5
+print(min(list))
+print(max(list))
+print(sorted(list))
+b = [4/3, 2/3, 1/3, 1/3, 1/3]
+print(sum(b))
+print(len(b))
+print(type(b))
--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 def hello():
+        '''prints  hello world'''
         print("hello world")
 
 def helloname(name):
@@ -14,6 +15,14 @@ def rec(no):
         if no==0:
                 return 0
         else:return rec(no-1)+no
+
+
+
+def defaultarg(test="aditya"):
+        print("hello",test)
+
+def multipleargs(arg1, arg2="hello"):
+        print(arg1,arg2)
 hello()
 
 helloname("aditya")
@@ -27,3 +36,8 @@ print(returnname())
 
 
 print(rec(5))
+print(defaultarg("cow"))
+print(defaultarg())
+
+multipleargs(arg2="hello",arg1="world")
+print(hello.__doc__ )
